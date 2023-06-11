bcc6106ddd46cc78bcdbf305aeacade0c141a60a
cerberus666 <cerberusofdeath@gmail.com>
2023-06-11 16:41:19 +0800

    simple game header

---
+++ Game.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#define Game_hpp
+
+#include "SDL.h"
+#include <iostream>
+
+class Game {
+public:
+	Game();
+	~Game();
+
+	void init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen);
+
+	void handleEvents();
+	void update();
+	void render();
+	void clean();
+
+	bool running();
+
+private:
+	bool isRunning;
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
