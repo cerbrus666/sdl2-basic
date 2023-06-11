3143b674b024aefdef5f7e174e19e987fd4b8285
cerberus666 <cerberusofdeath@gmail.com>
2023-06-11 17:16:19 +0800

    clean up main

--- Game.cpp
+++ Game.cpp
@@ -22,14 +22,42 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 		renderer = SDL_CreateRenderer(window, -1, 0);
 		if (renderer) {
-			std::cout << "Renderer succesfully created!" < std::endl;
+			std::cout << "Renderer succesfully created!" << std::endl;
+			SDL_GetRenderDrawColor(renderer, 255, 255, 0, 255);
 		}
 		isRunning = true;
 	}
 	else {
 		isRunning = false;
+	}	
+}
+
+void Game::handleEvents() {
+	SDL_Event event;
+	SDL_PollEvent(&event);
+	switch (event.type) {
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+
+	default:
+		break;
 	}
+}
 
+void Game::update() {
 
-	
+}
+
+void Game::render() {
+	SDL_RenderClear(renderer);
+	// add stuff
+	SDL_RenderPresent(renderer);
+}
+
+void Game::clean() {
+	SDL_DestroyWindow(window);
+	SDL_DestroyRenderer(renderer);
+	SDL_Quit();
+	std::cout << "Game cleaned! " << std::endl;
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@ and may not be redistributed without written permission.*/
 //Using SDL and standard IO
 #include <SDL.h>
 #include <iostream>
+#include "Game.hpp"
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
@@ -16,39 +17,6 @@ int main(int argc, char* args[])
 
 	//The surface contained by the window
 	SDL_Surface* screenSurface = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0)
-	{
-		std::cout << ("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-	}
-	else
-	{
-		//Create window
-		window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-		if (window == NULL)
-		{
-			std::cout << ("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-		}
-		else
-		{
-			SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-
-			SDL_SetRenderDrawColor(renderer, 255, 0, 255, 200);
-
-			SDL_RenderClear(renderer);
-
-			SDL_RenderPresent(renderer);
-
-			SDL_Delay(3000);
-		}
-	}
-
-	//Destroy window
-	SDL_DestroyWindow(window);
-
-	//Quit SDL subsystems
-	SDL_Quit();
 
 	return 0;
 }
